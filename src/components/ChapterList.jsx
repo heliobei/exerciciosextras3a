1e9fa0d9a9e222de378ebaad32c6794e3aa79041
heliobei <suporte@beieducacaopedagogico.com>
2025-04-23 13:19:55 -0300

    Add files via upload

---
+++ src/components/ChapterList.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import ChapterCard from "./ChapterCard";
+
+const chapters = [
+  { id: 1, title: "Aprendendo a tomar decisões", teacherPdf: "/assets/pdfs/Educador_Capitulo_01_3a.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_01_3a.pdf" },
+  { id: 2, title: "Vale a pena uma pizza por esse preço?", teacherPdf: "/assets/pdfs/Educador_Capitulo_02_3a.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_02_3a.pdf" },
+  
+];
+
+const ChapterList = () => {
+  const [openPdf, setOpenPdf] = useState({ id: null, type: null });
+
+  const togglePdf = (id, type) => {
+    if (openPdf.id === id && openPdf.type === type) {
+      setOpenPdf({ id: null, type: null });
+    } else {
+      setOpenPdf({ id, type });
+    }
+  };
+
+  const downloadPdf = (path) => {
+    const link = document.createElement("a");
+    link.href = path;
+    link.download = path.split("/").pop();
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div className="w-full max-w-5xl mx-auto p-6" id="chapter-list">
+      <h1 className="text-4xl font-semibold text-center mb-4 bg-gradient-to-br from-[#18a4ac] to-[#21BDC6]  text-transparent bg-clip-text">
+        Biblioteca de Capítulos
+      </h1>
+      <p className="text-lg text-gray-600 text-center mb-8 max-w-3xl mx-auto pb-4">
+        Explore os capítulos abaixo e visualize / baixe os conteúdos do Caderno do Educador ou do Livro do Estudante.
+      </p>
+
+      {chapters.map((chapter) => (
+        <ChapterCard
+          key={chapter.id}
+          chapter={chapter}
+          openPdf={openPdf}
+          togglePdf={togglePdf}
+          downloadPdf={downloadPdf}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ChapterList;
